9fe5a0864f3bac946ac20b654465c38c4257b2e4
Ali Tariq <alibabapk@aol.com>
2024-07-28 12:36:24 +0500

    More services

--- src/main/java/org/acmeware/Application.java
+++ src/main/java/org/acmeware/Application.java
@@ -2,11 +2,11 @@ package org.acmeware;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome to app-ointment!");
+        SpringApplication.run(Application.class,args);
 
     }
 }
