056318a24811c44e5e26a30cd5baba6267d8f771
Ali Tariq <alibabapk@aol.com>
2024-08-10 05:57:24 +0500

    add auth service classes

---
+++ auth-service/src/main/java/controllers/AuthController.java
@@ -0,0 +1,29 @@
+package controllers;
+
+import entities.AuthRequest;
+import entities.AuthResponse;
+import lombok.AllArgsConstructor;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import services.AuthService;
+
+@RestController
+@RequestMapping("/auth")
+@AllArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping(value = "/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody AuthRequest request) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+}
---
+++ auth-service/src/main/java/services/AuthService.java
@@ -0,0 +1,34 @@
+package services;
+
+import entities.AuthRequest;
+import entities.AuthResponse;
+import entities.UserVO;
+import lombok.AllArgsConstructor;
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@AllArgsConstructor
+public class AuthService {
+
+    private final RestTemplate restTemplate;
+    private final JwtUtil jwtUtil;
+
+    public AuthService(RestTemplate restTemplate, JwtUtil jwtUtil) {
+        this.restTemplate = restTemplate;
+        this.jwtUtil = jwtUtil;
+    }
+
+    public AuthResponse register(AuthRequest request) {
+        //do validation if user exists in DB
+        request.setPassword(BCrypt.hashpw(request.getPassword(), BCrypt.gensalt()));
+        UserVO registeredUser = restTemplate.postForObject("http://user-service/users", request, UserVO.class);
+
+        String accessToken = jwtUtil.generate(registeredUser.getId(), registeredUser.getRole(), "ACCESS");
+        String refreshToken = jwtUtil.generate(registeredUser.getId(), registeredUser.getRole(), "REFRESH");
+
+        return new AuthResponse(accessToken, refreshToken);
+    }
+
+}
